b1cc8d7f9cf6232a0719f8879913d97b1aa5d218
SumonHossainJs <sumonhosainrakib@gmail.com>
2023-12-26 18:46:58 +0300

    feat(HeaderBrand.jsx): completed Al hamdulilah

--- src/components/header/element/HeaderBrand.jsx
+++ src/components/header/element/HeaderBrand.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image";
+import { Logo } from "@/Data/Common";
 
-const HeaderBrand = () => {
+const HeaderBrand = (props) => {
   return (
-    <div></div>
+    <div className="header-brand">
+      <Link href={"/"} className="logo">
+        <Image
+        src={props.light ? Logo.light : Logo.dark}
+        alt="Site Logo"
+        height={40}
+        width={150}
+        />
+      </Link>
+    </div>
   )
 }
 
--- src/components/splash/SplashHeader.jsx
+++ src/components/splash/SplashHeader.jsx
@@ -1,49 +1,73 @@
-"use client"
-import React, {useEffect, useRef} from 'react'
-import HeaderBrand from '../header/element/HeaderBrand';
-import HeaderTopNotify from '../header/element/HeaderTopNotify';
-
+"use client";
+import React, { useEffect, useRef } from "react";
+import HeaderBrand from "../header/element/HeaderBrand";
+import HeaderTopNotify from "../header/element/HeaderTopNotify";
 
 const SplashHeader = () => {
+  const axilPlaceholder = useRef();
+  const axilMainmenu = useRef();
 
-	const axilPlaceholder = useRef();
-	const axilMainmenu = useRef();
-  
-	useEffect(() => {
-	  const mainMenu = axilMainmenu.current;
-	  const mainMenuHeight = axilMainmenu.current.clientHeight;
-	  const mainmenuPlaceholder = axilPlaceholder.current;
-	  window.addEventListener("scroll", (event) => {
-		  if (window.scrollY > 40) {
-			  mainmenuPlaceholder.style.height = mainMenuHeight + 'px';
-			  mainMenu.classList.add("axil-sticky");
-		  } else {
-			  mainmenuPlaceholder.style.height = '0';
-			  mainMenu.classList.remove("axil-sticky");
-		  }
-	  });
-	}, []);
+  useEffect(() => {
+    const mainMenu = axilMainmenu.current;
+    const mainMenuHeight = axilMainmenu.current.clientHeight;
+    const mainmenuPlaceholder = axilPlaceholder.current;
+    window.addEventListener("scroll", (event) => {
+      if (window.scrollY > 40) {
+        mainmenuPlaceholder.style.height = mainMenuHeight + "px";
+        mainMenu.classList.add("axil-sticky");
+      } else {
+        mainmenuPlaceholder.style.height = "0";
+        mainMenu.classList.remove("axil-sticky");
+      }
+    });
+  }, []);
 
   return (
-    <header className='sumon-header header-style-3'>
+    <header className="sumon-header header-style-3">
       <HeaderTopNotify bgImage="/images/others/campaign-bg2.png">
         <p>
-          Introductory Offer Get Upto 50% Off { ''}
-          <a
-            target="_blank"
-            href="#"
-          >
+          Introductory Offer Get Upto 50% Off {""}
+          <a target="_blank" href="#">
             Buy Now
           </a>
         </p>
       </HeaderTopNotify>
-      <div className="sumon-sticky-placeholder" ref={axilPlaceholder}/>
+      <div className="sumon-sticky-placeholder" ref={axilPlaceholder} />
       <div className="axil-mainmenu" ref={axilMainmenu}>
-        Al ahmdulilah
-        <i className='bx bx-x'></i>
+        <div className="container">
+          <div className="header-navbar">
+            <HeaderBrand />
+            <div className="header-main-nav">
+              <nav className="mainmenu-nav">
+                <ul className="mainmenu">
+                  <li>
+                    <a href="#demos" className="smoth-animation">
+                      Demos
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#features" className="smoth-animation">
+                      Features
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#documentation" className="smoth-animation">
+                      Documentation
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#support" className="smoth-animation">
+                      Support
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
       </div>
-      </header>
-  )
-}
+    </header>
+  );
+};
 
-export default SplashHeader
+export default SplashHeader;
