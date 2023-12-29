a6590f362426a4e8d9ca5b2ddfa1ec94c61d2605
SumonHossainJs <sumonhosainrakib@gmail.com>
2023-12-29 20:43:49 +0300

    feat(SplashBanner.jsx): started working

--- src/components/splash/SplashBanner.jsx
+++ src/components/splash/SplashBanner.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+"use client"
+import Image from "next/image"
+import CountUp from "react-countup"
+import TrackVisibility from "react-on-screen"
 
 const SplashBanner = () => {
+
+  
   return (
     <div>SplashBanner</div>
   )
