7b75f9896e96070b61d5f0e6b743afab6adb128f
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-01-01 21:35:56 +0300

    Al hamdulilah Subhan allah

--- src/components/splash/SplashFeatures.jsx
+++ src/components/splash/SplashFeatures.jsx
@@ -1,6 +1,40 @@
-import React from 'react'
+import React from 'react';
+import SectionTitle from '../element/SectionTitle';
+
 
 const SplashFeatures = () => {
+  const Themefeatures = [
+    {
+      title:"powered by Next.js",
+      icon:"/images/preview/features-icon-10.png",
+      text:'Our Template is Perfect for all devices. It is made by Next.js Structure. Next.js was designed to provide high performance in mind.'
+    },
+    {
+      title:"Reusable Compnents",
+      icon:"/images/preview/features-icon-11.png",
+      text:'Our Template is Perfect for all devices. It is made by Next.js Structure. Next.js was designed to provide high performance in mind.'
+    },
+    {
+      title:"Fast Loading speed",
+      icon:"/images/preview/features-icon-05.png",
+      text:'Our Template is Perfect for all devices. It is made by Next.js Structure. Next.js was designed to provide high performance in mind.'
+    },
+    {
+      title:"Fully Responsive <br/> Layout",
+      icon:"/images/preview/features-icon-01.png",
+      text:'Our Template is Perfect for all devices. It is made by Next.js Structure. Next.js was designed to provide high performance in mind.'
+    },
+    {
+      title:"25+ Resourceful <br/> Pages",
+      icon:"/images/preview/features-icon-02.png",
+      text:'Our Template is Perfect for all devices. It is made by Next.js Structure. Next.js was designed to provide high performance in mind.'
+    },
+    {
+      title:"Fully <br/> Customizable",
+      icon:"/images/preview/features-icon-02.png",
+      text:'Our Template is Perfect for all devices. It is made by Next.js Structure. Next.js was designed to provide high performance in mind.'
+    },
+  ]
   return (
     <div>SplashFeatures</div>
   )
--- src/components/splash/SplashInnerDemo.jsx
+++ src/components/splash/SplashInnerDemo.jsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import SectionTitle from '../element/SectionTitle';
+import Image from 'next/image';
+
 
 const SplashInnerDemo = () => {
+  const InnerDemo =[
+    {
+      title: "Shop",
+      thumb: "/images/preview/shop.png",
+      url: "#"
+  },
+  {
+      title: "Shop With Sidebar",
+      thumb: "/images/preview/shop-sidebar.png",
+      url: "#"
+  },
+  {
+      title: "Product Variation 1",
+      thumb: "/images/preview/variation-1.png",
+      url: "#"
+  },
+  {
+      title: "Product Variation 2",
+      thumb: "/images/preview/variation-2.png",
+      url: "#"
+  },
+  {
+      title: "Product Variation 3",
+      thumb: "/images/preview/variation-3.png",
+      url: "#"
+  },
+  {
+      title: "Product Variation 4",
+      thumb: "/images/preview/variation-4.png",
+      url: "#"
+  },
+  {
+      title: "Product Variation 5",
+      thumb: "/images/preview/variation-5.png",
+      url: "#"
+    },
+    
+  ]
   return (
-    <div>SplashInnerDemo</div>
+    <div className='axil-section-gap pv-demo-area'>
+      <div className="container">
+        <SectionTitle
+        pClass="section-title-center"
+        subtitle='Inner Pages'
+        subtitleIcon="box bx-fire"
+        title='Eye catching Pre-build <br/> Inner pages'
+        />
+        <div className="row">
+          {InnerDemo.map((data, index)=>(
+            <div className="col-lg-4 col-md-6 col-12" key={index}>
+              <div className="pv-single-demo">
+                <div className="thumb box">
+                  <Image
+                  src={data.thumb}
+                  width={410}
+                  height={410}
+                  alt={data.title}
+                  />
+                  <Link href={data.url} className='axil-btn btn-bg-primary right-icon view-btn'> View Demo <i className='bx bx-right-arrow-alt'></i>
+                  </Link>
+                </div>
+                <h5 className='title'>
+                  <Link href={data.url}>{data.title}</Link>
+                </h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </div>
   )
 }
 
