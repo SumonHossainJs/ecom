aa7707587c85be6d69dd30d8b7f7c9c0f2a50a8e
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-01-02 20:46:37 +0300

    feat( splashSupport.jsx):completed

--- src/components/splash/SplashCta.jsx
+++ src/components/splash/SplashCta.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import SectionTitle from "../element/SectionTitle";
+import Image from "next/image";
 
 const SplashCta = () => {
   return (
-    <div>SplashCta</div>
-  )
-}
+    <div className="pv-call-to-action">
+      <div className="container">
+        <div className="pv-action-box">
+          <div className="section-title-wrapper">
+            <SectionTitle
+              pClass="section-title-center"
+              subtitle="Are You ready"
+              subtitleIcon="bx bxs-hot"
+              title="Let's Start Your Business today!"
+            />
+            <a href="#" className=" axil-btn btn-bg-primary su-btn">
+              <i className="box box cart"></i> Buy now
+            </a>
+          </div>
+          <div className="pv-action-thumbnail">
+            <Image
+            src={"/images/preview/cta-mockup.png"}
+            width={1230}
+            height={540}
+            alt="cta Mockup"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SplashCta
+export default SplashCta;
--- src/components/splash/SplashSupport.jsx
+++ src/components/splash/SplashSupport.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
 
 const SplashSupport = () => {
+  const SupportData =[
+    {
+      title:"Online Documentation", 
+      icon:"Booki",
+      text:"Well organized and up to date",
+      btnText:"View Documentation",
+      btnUrl:"#",
+      class:"online-documentation",
+    },
+    {
+      title:"Dedicated Support", 
+      icon:"headset",
+      text:"We Respond within 1 business day. Weekends excluded",
+      btnText:"contact Support",
+      btnUrl:"#",
+      class:"dedicated-support",
+    },
+  ]
   return (
-    <div>SplashSupport</div>
+    <div className='pv-support-area'>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-10">
+            <div className="row row--20">
+              {SupportData.map((data,index)=>(
+                <div className="col-lg-6" key={index}>
+                  <div className={`pv-support ${data.class}`} >
+                    <a href={data.btnUrl} target="_blank" rel="noopener noreferrer" className='inner'>
+                      <span className="icon">
+                        <i className={data.icon}></i>
+                      </span>
+                      <span className="content">
+                        <div className="h3 title">
+                          {data.title}
+                        </div>
+                        <span className="b1 paragraph">{data.text}</span>
+                        <span className="axil-btn">
+                          {data.btnText}
+                          <i className="box bsx-arrow"></i>
+                        </span>
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
