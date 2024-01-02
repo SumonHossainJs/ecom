c3142f6bd185dbb588e7872e38edb2810cb7dff3
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-01-02 20:55:00 +0300

    feat(splashPage):completed

--- src/components/splash/SplashFooter.jsx
+++ src/components/splash/SplashFooter.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const SplashFooter = () => {
   return (
-    <div>SplashFooter</div>
+    <div className='axil-footer-area Footer-style-2 pv-footer-area'>
+      <div className="copyright-area copyright-default">
+        <div className="container">
+          <div className="copyright-left d-flex flex-wrap justify-content-center">
+            <ul className='quick-link'>
+              <li>
+              © {new Date().getFullYear()}. All rights reserved by {" "} Sumon hosain Rekib
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
