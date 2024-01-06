631c5cb46e51e0c958c71cc96d06b806c973c2ea
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-01-06 22:07:33 +0300

    done for today

--- src/components/header/elements/HeaderQuickLink.jsx
+++ src/components/header/elements/HeaderQuickLink.jsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HeaderQuickLink = () => {
   return (
-    <div>HeaderQuickLink</div>
+    <div className='header-top-link'>
+      <ul className="quick-link">
+        <li>
+          <Link href="/sign-up">Join us</Link>
+        </li>
+        <li>
+          <Link href="/sign-in">Sign in</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
