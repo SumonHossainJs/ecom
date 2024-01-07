515990651514de83786c0adc23e9569986d76c61
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-01-07 21:09:13 +0300

    feat( nav.jsx):started working

--- src/components/header/elements/Nav.jsx
+++ src/components/header/elements/Nav.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+'use client'
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+ import { useSelector, useDispatch } from "react-redux";
+import { HeaderMenu } from "@/Data/Menu";
+import { mobileMenu } from "@/store/slices/menuSlice";
 
 const Nav = () => {
+  const dispatch = useDispatch();
+  const menuOption = useSelector((state)=> state.menu);
+const [windowWidth, setWindowWidth] = useState();
+
+const mobileMenuHandler = (data) =>{
+  dispatch(mobileMenu(data));
+}
   return (
     <div>Nav</div>
   )
