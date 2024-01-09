e0f62d5c15a007ac5b1623223e6c29be6274a740
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-01-09 19:58:44 +0300

    feat(productSearchModal.jsx)

--- src/components/header/elements/ProductSearchModal.jsx
+++ src/components/header/elements/ProductSearchModal.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+"use client"
+import Link from "next/link"
+import { useState } from "react"
+import ProductsData from "@/Data/Products";
+import ProductPrice from "@/components/product/elements/ProductPrice";
+import ProductRating from "@/components/product/elements/ProductRating";
+import ProductThumbnail from "@/components/product/elements/ProductThumbnail";
+import ProductTitle from "@/components/product/elements/ProductTitle";
+import ActionButton from "@/components/product/elements/ActionButton";
 
 const ProductSearchModal = () => {
   return (
