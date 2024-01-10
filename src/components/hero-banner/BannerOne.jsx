6420d150778d638a50867ed7449eea8c85a8e171
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-01-10 21:51:50 +0300

    feat(bannerOne.jsx):Al hamduliliah Started Working on it

--- src/components/hero-banner/BannerOne.jsx
+++ src/components/hero-banner/BannerOne.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+'use client'
+import SlickSlider from "../element/SlickSlider";
+import { ElectronicsSlider } from "@/Data/Slider";
 
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 const BannerOne = () => {
+  const [nav1, setNav1] = useState();
+  const [nav2, setNav2] = useState();
   return (
     <div>BannerOne</div>
   )
